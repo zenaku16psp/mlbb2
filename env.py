02ad3892f52c5732f2b846af6cc6eac61e48a5d6
zenaku16psp <pyaesone5p.s.p@gmail.com>
2025-10-23 20:49:38 +0630

    Update env.py

--- env.py
+++ env.py
@@ -11,7 +11,7 @@ except FileNotFoundError:
     pass
 
 # Bot configuration
-BOT_TOKEN = (os.getenv("BOT_TOKEN","7988925435:AAE30e4vC5A2vHJoahphAA2bjsoFsyd8gJA"))
-ADMIN_ID = int(os.getenv("ADMIN_ID", "8197491717"))
-ADMIN_GROUP_ID = int(os.getenv("ADMIN_GROUP_ID", "-1002921388318"))
+BOT_TOKEN = (os.getenv("BOT_TOKEN","8437367286:AAFMJMWurNe154x-M6r_PzB9z2pxOl_xmbU"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "6419935994"))
+ADMIN_GROUP_ID = int(os.getenv("ADMIN_GROUP_ID", "-1003033780543"))
 DATA_FILE = "data.json"
