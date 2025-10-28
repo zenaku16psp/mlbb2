ce36ab6a1fb8f69bc5c183cd6e2d11f3d51fd7b5
zenaku16psp <pyaesone5p.s.p@gmail.com>
2025-10-28 11:59:44 +0630

    Update env.py

--- env.py
+++ env.py
@@ -15,3 +15,4 @@ BOT_TOKEN = (os.getenv("BOT_TOKEN","8437367286:AAFMJMWurNe154x-M6r_PzB9z2pxOl_xm
 ADMIN_ID = int(os.getenv("ADMIN_ID", "6419935994"))
 ADMIN_GROUP_ID = int(os.getenv("ADMIN_GROUP_ID", "-1003033780543"))
 DATA_FILE = "data.json"
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://wanglin_db_user:wanglin@wanglin.afzj10f.mongodb.net/")
